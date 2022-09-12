36a58876cdcd5ecc64051d1ebd65df294648388d
ARIQFADHLURRAHMAN <fadhlurrahmanariq51@gmail.com>
2022-09-12 11:36:54 +0700

    room_hotel

--- resources/views/room.blade.php
+++ resources/views/room.blade.php
@@ -1,22 +1,45 @@
 @extends('layouts.main')
 
 @section('container')
-     <div>
-        <label class="px col-sm-2 col-from-label">Tanggal Cek In</label>
-                <div class="col-sm-2">
-                    <input type="text" class="form-control" placeholder="">
-                </div>
-                <label class="col-sm-2 col-from-label">Tanggal Cek Out</label>
-                <div class="col-sm-2">
-                    <input type="date" class="form-control" placeholder=".col-4">
-                </div>
-                <label class="col-sm-2 col-from-label">Jumlah Kamar</label>
-                <label class="col-sm-2 col-from-label">Jumlah Kamar</label>
-                <div class="col-sm-1">
-                    <input type="date" class="form-control" placeholder=".col-3">
-                </div>
-            </div> 
+<div class="room">
+    <div class="judul" >
+        <h3>Make A Reservasition</h3>
+    </div>
+    
+    <div class="col-sm-5">
+        <label class="px col-sm-3 col-from-label">Number Of room</label>
+        <input type="text" class="form-control" placeholder="Number room">
+    </div>
 
-            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <div class="col-sm-5">
+        <label class="col-sm-3 col-from-label">Name</label>
+        <input type="text" class="form-control" placeholder="Name">
+    </div>
+
+    <div class="col-sm-5">
+        <label class="col-sm-5 col-from-label">Phone Number</label>
+        <input type="number" class="form-control" placeholder="+62">
+    </div>
+
+    <div class="col-sm-2">
+        <label class="col-sm-5 col-from-label">Check-in date</label>
+        <input type="date" class="form-control" placeholder="+62">    
+    </div>
+
+    <div class="col-sm-2">
+        <label class="col-sm-6 col-from-label">Check-out date</label>
+        <input type="date" class="form-control" placeholder="+62">
+    </div>
+    
+    <div class="col-sm-5">
+        <label class="col-sm-5 col-from-label">payment</label>
+        <input type="text" class="form-control" placeholder="">
+    </div>
+</div>
+
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
+</script>
 @endsection
