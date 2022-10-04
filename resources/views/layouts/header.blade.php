45012b8399c7e257d528af05bff7a3bc8ece71b8
ahmadhilmy123 <tomohilmy@gmail.com>
2022-10-04 10:00:47 +0700

    tambah_kamar

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -12,11 +12,10 @@
           <li class="nav-item">
             <a class="nav-link {{ Request::is('room') ? 'active' : '' }}" href="/room">Room</a>
           </li>
-          @if(auth()->user()->role == 'admin')
+           {{-- @if(auth()->user()->role == 'admin') --}}
           <li class="nav-item">
             <a class="nav-link {{ Request::is('facility') ? 'active' : '' }}" href="/administrator">Admin</a>
           </li>
-          @endif
         </ul>
         <form class="d-flex col-8" role="search" method>
           <input class="form-control mx-3" type="search" placeholder="Search" aria-label="Search">
@@ -25,3 +24,4 @@
       <a href="/login" class="nav-link text-white" style="margin-left: 10px"><i class="bi bi-box-arrow-in-left"></i> Logout</a>
     </div>
   </nav>
+  {{-- @endif --}}
--- resources/views/room.blade.php
+++ resources/views/room.blade.php
@@ -1,67 +1,46 @@
 @extends('layouts.main')
 
 @section('container')
-<div class="room">
-    <main class="form-registration">
-        <h1 class="h3 mb-3 fw-normal text-center mt-2">Make A Reservasition</h1>
-        <a type="button" href="/tambah_room" class="btn btn-success">Tambah +</a>
-    <div class="container1" style="display: flex">
-        <div class="container2" style="width: 100%;">
-            <div class="col-sm-6">
-                <label class="px col-sm-5 col-from-label">Number Of room</label>
-                <input type="text" class="form-control" placeholder="Number room">
-            </div>
-    
-            <div class="col-sm-6">
-                <label class="col-sm-3 col-from-label">Name</label>
-                <input type="text" class="form-control" placeholder="Name">
-            </div>
-    
-            <div class="col-sm-6">
-                <label class="col-sm-5 col-from-label">Phone Number</label>
-                <input type="number" class="form-control" placeholder="+62">
-            </div>
-        </div>
-
-        <div class="container3" style="display: flex">
-            <div class="container9" style="width: 600px">
-            <div class="col-sm-6">
-                <label class="col-sm-20 col-from-label">Check-in date</label>
-                <input type="date" class="form-control" placeholder="+62">
-            </div>
-    
-            <div class="col-sm-6">
-                <label class="col-sm-15 col-from-label">Check-out date</label>
-                <input type="date" class="form-control" placeholder="+62">
-            </div>
-    
-            <div class="col-sm-6">
-                <label class="col-sm-5 col-from-label">payment</label>
-                <input type="text" class="form-control" placeholder="">
-            </div>
+<div class="container pt-2">
+    <div class="judul mb-2">
+        <h1 class="text-center">kamar</h1>
+        {{-- <a type="button" href="/tambah_room" class="btn btn-success">Tambah +</a> --}}
+    </div>
+    <table class="table table-bordered">
+        <thead>
+            <tr>
+                <th scope="col">ID</th>
+                <th scope="col">Nama</th>
+                <th scope="col">tipe kamar</th>
+                <th scope="col">Jumblah Kamar</th>
+                <th scope="col">Tanggal Chekin</th>
+                <th scope="col">Tanggal Chekout</th>
+            </tr>
+        </thead>
+        <tbody>
             <br>
             @foreach ($data as $row)
             <tr>
                 <th scope="row">{{ $loop->iteration }}</th>
                 <td>{{ $row->user_id }}</td>
                 <td>{{ $row->kamar_id }}</td>
+                <td>{{ $row->jumblah_kamar }}</td>
                 <td>{{ $row->tanggal_checkin }}</td>
                 <td>{{ $row->tanggal_checkout }}</td>
-                <td>{{ $row->payment }}</td>
-                <td class="d-flex">
-                    <form action="/delete_room/{{ $row->id }}" method="POST" style="margin-right:20px">
+                {{-- <td class="d-flex"> --}}
+                    {{-- <form action="/delete_room/{{ $row->id }}" method="POST" style="margin-right:20px">
                         @csrf
                         @method('delete')
                         <button type="submit" class="btn btn-danger me-4">Delete</button>
                     </form>
                     <a type="button" href="/tampilan_room{{ $row->id }}"
-                        class="btn btn-warning mr-2">Edit</a>
-                </td>
-            </tr>
+                        class="btn btn-warning mr-2">Edit</a> --}}
+                {{-- </td>
+            </tr> --}}
             @endforeach
-    </div>
+    {{-- </div>
 </div>
-</div>
+</div> --}}
 
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
