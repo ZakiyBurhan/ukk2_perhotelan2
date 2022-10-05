d7769293a861dfe78d389475e596e133319a05c7
ahmadhilmy123 <tomohilmy@gmail.com>
2022-10-05 13:08:57 +0700

    login

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,5 +1,292 @@
 @extends('layouts.main')
 
 @section('container')
+<div class="container">
+    <img class="d-block w-100 mt-5" src="/assets/kamar.jpg" alt="first slide" height="400">
+</div>
+<div class="container5">
+    <div class="d">
+        <center>
+            <h1>Rekomendasi</h1>
+        </center>
+    </div>
+    <div class="profile-container" style="display: flex; justify-content: space-around">
+        <div class="card" style="width: 400px">
+            <img src="/assets/kamar.jpg" alt="" class="card-img">
+            <div class="card-body">
+                <h2>Hotel bandung</h2>
+                <p>Rp 150.000</p>
+                <div class="icon">
+                    <a type="button" href="/tambah_room" class="btn btn-danger">Pesan </a>
+                </div>
+            </div>
 
+        </div>
+        <div class="card" style="width: 400px">
+            <img src="/assets/kamar.jpg" alt="" class="card-img">
+            <div class="card-body">
+                <h2>Hotel Jakarta</h2>
+                <p>Fasilitas Lengkap</p>
+                <div class="icon">
+                    <a type="button" href="/tambah_room" class="btn btn-danger">Pesan </a>
+                </div>
+            </div>
+        </div>
+        <div class="card" style="width: 400px">
+            <img src="/assets/kamar.jpg" alt="" class="card-img">
+            <div class="card-body">
+                <h2>Hotel Semarang</h2>
+                <p>Fasilitas Lengkap</p>
+                <div class="icon">
+                    <a type="button" href="/tambah_room" class="btn btn-danger">Pesan </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="container5">
+    <div class="d">
+        <br>
+    </div>
+    <div class="profile-container" style="display: flex; justify-content: space-around">
+        <div class="card" style="width: 400px">
+            <img src="/assets/kamar.jpg" alt="" class="card-img">
+            <div class="card-body">
+                <h2>Hotel Surabaya</h2>
+                <p>Fasilitas Lengkap</p>
+                <div class="icon">
+                    <a type="button" href="/tambah_room" class="btn btn-danger">Pesan </a>
+                </div>
+            </div>
+
+        </div>
+        <div class="card" style="width: 400px">
+            <img src="/assets/kamar.jpg" alt="" class="card-img">
+            <div class="card-body">
+                <h2>Hotel Malang</h2>
+                <p>Fasilitas Lengkap</p>
+                <div class="icon">
+                    <a type="button" href="/tambah_room" class="btn btn-danger">Pesan </a>
+                </div>
+            </div>
+        </div>
+        <div class="card" style="width: 400px">
+            <img src="/assets/kamar.jpg" alt="" class="card-img">
+            <div class="card-body">
+                <h2>Hotel Bogor</h2>
+                <p>Fasilitas Lengkap</p>
+                <div class="icon">
+                    <a type="button" href="/tambah_room" class="btn btn-danger">Pesan </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
+<br>
+<div class="container5">
+    <div class="d">
+        <center>
+            <h1>Fasilitas Hotel</h1>
+        </center>
+    </div>
+    <div class="profile-container" style="display: flex; justify-content: space-around">
+        <div class="mt" style="width: 400px">
+            <img src="/assets/kolamrenang.jpg" alt="" class="card-img">
+            <div class="card-body">
+                <div class="icon">
+                </div>
+            </div>
+        </div>
+        <div class="mt" style="width: 1000px">
+            <div class="card-body">
+                <h2>Kolam Renang</h2>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error, tempore qui sequi temporibus sed veritatis facere voluptas commodi nisi. Doloremque, alias unde in harum repudiandae ad sunt? Totam exercitationem, nobis sint reiciendis odit laborum, eum nesciunt a laudantium obcaecati dolores tempora iusto sequi minus aliquam natus corrupti! Harum, ex maiores.</p>
+                <div class="icon">
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+    <div class="profile-container" style="display: flex; justify-content: space-around">
+        <div class="mt" style="width: 400px">
+            <img src="/assets/olahraga.jpg" alt="" class="card-img">
+            <div class="card-body">
+                <div class="icon">
+                </div>
+            </div>
+        </div>
+        <div class="mt" style="width: 1000px">
+            <div class="card-body">
+                <h2>Alat Olahraga</h2>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error, tempore qui sequi temporibus sed veritatis facere voluptas commodi nisi. Doloremque, alias unde in harum repudiandae ad sunt? Totam exercitationem, nobis sint reiciendis odit laborum, eum nesciunt a laudantium obcaecati dolores tempora iusto sequi minus aliquam natus corrupti! Harum, ex maiores.</p>
+                <div class="icon">
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+    <div class="profile-container" style="display: flex; justify-content: space-around">
+        <div class="mt" style="width: 400px">
+            <img src="/assets/sofa.jpg" alt="" class="card-img">
+            <div class="card-body">
+                <div class="icon">
+                </div>
+            </div>
+        </div>
+        <div class="mt" style="width: 1000px">
+            <div class="card-body">
+                <h2>Tempat Bersantai</h2>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error, tempore qui sequi temporibus sed veritatis facere voluptas commodi nisi. Doloremque, alias unde in harum repudiandae ad sunt? Totam exercitationem, nobis sint reiciendis odit laborum, eum nesciunt a laudantium obcaecati dolores tempora iusto sequi minus aliquam natus corrupti! Harum, ex maiores.</p>
+                <div class="icon">
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+    <div class="profile-container" style="display: flex; justify-content: space-around">
+        <div class="mt" style="width: 400px">
+            <img src="/assets/spa.jpg" alt="" class="card-img">
+            <div class="card-body">
+                <div class="icon">
+                </div>
+            </div>
+        </div>
+        <div class="mt" style="width: 1000px">
+            <div class="card-body">
+                <h2>Spa</h2>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error, tempore qui sequi temporibus sed veritatis facere voluptas commodi nisi. Doloremque, alias unde in harum repudiandae ad sunt? Totam exercitationem, nobis sint reiciendis odit laborum, eum nesciunt a laudantium obcaecati dolores tempora iusto sequi minus aliquam natus corrupti! Harum, ex maiores.</p>
+                <div class="icon">
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+    <div class="profile-container" style="display: flex; justify-content: space-around">
+        <div class="mt" style="width: 400px">
+            <img src="/assets/dinner.jpg" alt="" class="card-img">
+            <div class="card-body">
+                <div class="icon">
+                </div>
+            </div>
+        </div>
+        <div class="mt" style="width: 1000px">
+            <div class="card-body">
+                <h2>Lestoran</h2>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error, tempore qui sequi temporibus sed veritatis facere voluptas commodi nisi. Doloremque, alias unde in harum repudiandae ad sunt? Totam exercitationem, nobis sint reiciendis odit laborum, eum nesciunt a laudantium obcaecati dolores tempora iusto sequi minus aliquam natus corrupti! Harum, ex maiores.</p>
+                <div class="icon">
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<style type="text/css">
+
+.footer{
+    background-color: #181111;
+    padding-top: 20px;
+    margin-top: 50px;
+}
+
+.footer-atas{
+    display: flex;
+    margin: 50px auto;
+    width: 90%;
+}
+
+.atas-kiri{
+    width: 30%;
+    align-items: center;
+}
+
+.atas-kiri img{
+    height: 120px;
+    padding: 40px;
+}
+
+.logo-footer{
+    display: flex;
+    gap: 15px;
+    justify-content: center;
+}
+
+.atas-kanan{
+    width: 70%;
+    text-align: right;
+    color: white;
+    padding-top: 100px;
+}
+
+.atas-kanan h1{
+    margin-bottom: 20px;
+}
+
+.footer-bawah{
+    width: 65%;
+    margin: 50px auto 0px;
+    text-align: center;
+    padding-bottom: 60px;
+    color: white;
+}
+
+.fa{
+    padding: 10px;
+    font-size: 21px;
+    width: 20px;
+    text-align: center;
+    text-decoration: none;
+    margin: 5px 2px;
+}
+
+.fa-facebook{
+    background-color: #987f3b;
+    color: white;
+}
+.fa-twitter{
+    background-color: #55acee;
+    color: white;
+}
+.fa-google{
+    background-color: #dd4b39;
+    color: white;
+}
+.fa-instagram{
+    background-color: #125688;
+    color: white;
+}
+</style>
+<div class="footer">
+    <div class="footer-atas">
+        <div class="atas-kiri">
+            <div>
+                <img src="img/logo.png" alt="">
+            </div>
+            <div class="logo-footer">
+                <a href="" class="fa fa-facebook"></a>
+                <a href="" class="fa fa-twitter"></a>
+                <a href="" class="fa fa-google"></a>
+                <a href="" class="fa fa-instagram"></a>
+            </div>
+        </div>
+
+        <div class="atas-kanan">
+            <h1>kunjungi situs lainnya</h1>
+            <h5>kebijakan privasi | kebijakan cookie | syarat dan kententuan | hubungi kami</h5>
+        </div>
+    </div>
+    <hr>
+    <div class="footer-bawah">
+        <p>This site is the official website of the Ministry of Tourism and Creative Economy of the Republic of Indonesia. All content listed on this site is for informational purposes and not for commercial purposes. The displayed sale is a sign of a partnership that will connect you to Our Partners.</p>
+    </div>
+</div>
+
+        
+
+
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
+</script>
 @endsection
